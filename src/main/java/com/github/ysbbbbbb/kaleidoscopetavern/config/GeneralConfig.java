ef1053396486f930b153f5a42092d3fe65b0ec78
tartaric_acid <baka943@qq.com>
2026-05-15 17:29:37 +0800

    添加配置，禁用无限熔岩

--- src/main/java/com/github/ysbbbbbb/kaleidoscopetavern/config/GeneralConfig.java
+++ src/main/java/com/github/ysbbbbbb/kaleidoscopetavern/config/GeneralConfig.java
@@ -12,11 +12,17 @@ public class GeneralConfig {
     // 不能压汁的物品受到挤压时,是否掉出盆外
     public static ForgeConfigSpec.BooleanValue PRESSING_TUB_DROP_CONTENTS_ON_NON_JUICEABLE;
 
+    // 是否禁用龙头的无限熔岩
+    public static ForgeConfigSpec.BooleanValue INFINITE_LAVA_FROM_TAP;
+
     private static void general(ForgeConfigSpec.Builder builder) {
         builder.push("tavern");
 
         builder.comment("Whether the Pressing Tub drops its contents when a non-juiceable item is pressed.");
         PRESSING_TUB_DROP_CONTENTS_ON_NON_JUICEABLE = builder.define("PressingTubDropContentsOnNonJuiceable", true);
+
+        builder.comment("Whether to enable infinite lava from the tap. If enabled, the tap will provide infinite lava when attached to a lava cauldron.");
+        INFINITE_LAVA_FROM_TAP = builder.define("InfiniteLavaFromTap", true);
 
         builder.pop();
     }
